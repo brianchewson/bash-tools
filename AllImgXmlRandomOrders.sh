0bbcc26d7d768d603cbccf5544f03f77f7f095d8
Brian Hewson <brian.hewson@cd-adapco.com>
2017-03-23 10:13:41 -0400

    Updates: no launching subshell=removes empty from lines, clean up the first from by adding the final filename as the first from

--- AllImgXmlRandomOrders.sh
+++ AllImgXmlRandomOrders.sh
@@ -1,37 +1,45 @@
-#!/bin/sh -ex
+#!/bin/sh -e
 
-rm $1
+XML=$1
+rm $XML
+RANDOM_ORDER_FILE=t.rand
 
 P=`pwd`
 
-echo "<background>" >> $1
-echo "   <starttime>" >> $1
-echo "      <year>2009</year>" >> $1
-echo "      <month>08</month>" >> $1
-echo "      <day>04</day>" >> $1
-echo "      <hour>00</hour>" >> $1
-echo "      <minute>00</minute>" >> $1
-echo "      <second>00</second>" >> $1
-echo "   </starttime>" >> $1
-echo "<!-- This animation will start at midnight. -->" >> $1
+echo "<background>" >> $XML
+echo "   <starttime>" >> $XML
+echo "      <year>2009</year>" >> $XML
+echo "      <month>08</month>" >> $XML
+echo "      <day>04</day>" >> $XML
+echo "      <hour>00</hour>" >> $XML
+echo "      <minute>00</minute>" >> $XML
+echo "      <second>00</second>" >> $XML
+echo "   </starttime>" >> $XML
+echo "<!-- This animation will start at midnight. -->" >> $XML
 
 gore=""
 SIZE=$(shuf -i 1-100 -n 1)
 
 for (( i=0; i<$SIZE; i++ )); do
-  ls -1 | grep -v xml | grep -v '\.sh' | sort -R | while read line; do
-    echo "   <transition>" >> $1
-    echo "      <duration>0.1</duration>" >> $1
-    echo "      <from>$P/$gore</from>" >> $1
-    echo "      <to>$P/$line</to>" >> $1
-    echo "   </transition>" >> $1   
-    echo "   <static>" >> $1
-    echo "      <duration>$(shuf -i 1-100 -n 1)</duration>" >> $1
-    echo "      <file>$P/$line</file>" >> $1
-    echo "   </static>" >> $1 
-    gore=$line  
+  ls -1 > ${RANDOM_ORDER_FILE}
+  for FILE in xml \.sh ${RANDOM_ORDER_FILE}; do 
+    sed -i "/${FILE}/d" ${RANDOM_ORDER_FILE}
   done
-  gore=$line  
-done
+  sort -R ${RANDOM_ORDER_FILE} -o ${RANDOM_ORDER_FILE}
 
-echo "</background>" >> $1
+  while read line; do
+    echo "   <transition>" >> $XML
+    echo "      <duration>0.1</duration>" >> $XML
+    echo "      <from>$P/$gore</from>" >> $XML
+    echo "      <to>$P/$line</to>" >> $XML
+    echo "   </transition>" >> $XML   
+    echo "   <static>" >> $XML
+    echo "      <duration>$(shuf -i 1-100 -n 1)</duration>" >> $XML
+    echo "      <file>$P/$line</file>" >> $XML
+    echo "   </static>" >> $XML 
+    gore=$line  
+  done < ${RANDOM_ORDER_FILE}
+done
+echo "</background>" >> $XML
+
+sed -i "s#/<#/$gore<#" ${XML}
